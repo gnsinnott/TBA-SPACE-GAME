9ee9588423b8cdfb1b36b03776765fce4329f3bb
gnsinnott <84403298+gnsinnott@users.noreply.github.com>
2021-11-05 14:40:12 -0600

    Map class
    Add player location to map class and travel to function

--- mapDriver.cpp
+++ mapDriver.cpp
@@ -6,22 +6,30 @@ using namespace std;
 int main(){
     cout << "\033[1;31m\xF0\x9F\x9A\x80\033[0m" << endl;
     string filename = "cs.txt";
-    string location;
+    string destination;
     string nexLoc;
     Map map = Map();
+    bool playerMarked;
+    int column = 0;
+    int row = 0;
+    bool validLocation = false;
+    map.setFile(filename);
+    map.generateMap(filename);
+    playerMarked = map.setPlayerLoc(column,row);
     do {
-        map.setFile(filename);
-        map.generateMap(filename);
         map.printMap();
-        map.printLegend();
-        cout << "What map would you like to see?" << endl;
-        cin >> location;
-        for (int i = 0; i < map.getLocationCount(); i++){
-            if (map.getLocation(i).getMapKey() == location[0]){
-                filename = map.getLocation(i).getMapFile();
+        map.printLegend(playerMarked);
+        do {
+            cout << "What map would you like to go?" << endl << "You can enter a location legend key or a custom coordinate. (i.e. A2)" << endl;
+            cin >> destination;
+            if (1 == map.travelTo(destination)){
+                validLocation = true;
+
             }
         }
+        while(!validLocation);
+
     }
-    while(location != "Q");
+    while(destination != "Q");
     return 0;
 }
